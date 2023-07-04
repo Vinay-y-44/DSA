42def9ef60a0c1d1ba6f3d21e7b5b9b5da4498e0
Vinay-y-44 <vinay.y_inf2019@svce.edu.in>
2023-07-05 01:35:06 +0530

    this is the code of set mismatch

--- src/paatterns/Pattern1.java
+++ src/paatterns/Pattern1.java
@@ -3,11 +3,17 @@ package paatterns;
 public class Pattern1 {
 
     public static void main(String[] args) {
-        printP1(5);
+        patternP4(5);
     }
 
+
+//right angle triangl
+//  *
+//  * *
+//  * * *
+//  * * * *
+//  * * * * *
     static void printP1(int n){
-//right angle triangle
         for(int i=1;i<=n;i++){
             for(int j=1;j<=i;j++){
                 System.out.print("* ");
@@ -15,4 +21,44 @@ public class Pattern1 {
             System.out.println();
         }
     }
+    // right angle triangle with numbers
+//            1
+//            22
+//            333
+//            4444
+//            55555
+    static void printP2(int n){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+
+    static void patternP3(int n){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+    //print pattern
+    /*    * * * * *
+          * * * *
+          * * *
+          * *
+          *
+
+     */
+    static void patternP4(int n){
+        for(int i=n;i>=1;i--){
+            for(int j=1;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
 }
