358a81f21b5a4c0fa96b221ae2bb9ed808cb01ae
v1nay-y <93667784+v1nay-y@users.noreply.github.com>
2023-07-10 23:57:54 +0530

    Add files via upload

---
+++ src/Single_number.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+//https://leetcode.com/problems/single-number/description/
+public class Single_number {
+    public static void main(String[] args) {
+        int[] a = {4,1,2,1,2};
+        System.out.println(singleNumber(a));
+    }
+    public static int singleNumber(int[] nums) {
+        int ans=0;
+        for(int i=0; i<nums.length; i++){
+            ans ^= nums[i];  // ans = (ans) XOR (array element at i)
+        }
+        return ans;
+
+    }
+        }
